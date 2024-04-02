9db3c07d14f9dec77e1e8b8a0ef3cc50c5d3121a
feruzbek23 <feruzbek.dev@gmail.com>
2024-04-02 14:21:03 +0200

    fixed code

--- account/serializers.py
+++ account/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import UserProfile
+from account import models
 
 
 class UserResgistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserProfile
+        model = models.UserProfile
         fields = (
             "id",
             "username",
@@ -14,3 +14,20 @@ class UserResgistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only':True}
         }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
+        
+        return user
+    
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+ 
+        return super().update(instance, validated_data)
